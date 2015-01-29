4484e4d32f74c4691961d47d16b3c50aa6cbdba2
mikh <mikha.andreev@gmail.com>
2015-01-29 14:10:20 -0500

    testing assembly after change in read

--- Assignment 0/test_O_level.s
+++ Assignment 0/test_O_level.s
@@ -1,41 +1,32 @@
 	.file	"test_O_level.c"
-	.section	.rodata
+	.section	.rodata.str1.1,"aMS",@progbits,1
 .LC0:
 	.string	"\n Starting a loop "
 .LC1:
+	.string	"%d\n"
+.LC2:
 	.string	"\n done "
 	.text
 .globl main
 	.type	main, @function
 main:
-.LFB0:
+.LFB11:
 	.cfi_startproc
-	pushq	%rbp
+	subq	$8, %rsp
 	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$48, %rsp
-	movl	%edi, -36(%rbp)
-	movq	%rsi, -48(%rbp)
-	movq	$0, -8(%rbp)
 	movl	$.LC0, %edi
 	call	puts
-	movq	$0, -32(%rbp)
-	jmp	.L2
-.L3:
-	addq	$3, -8(%rbp)
-	addq	$1, -32(%rbp)
-.L2:
-	cmpq	$100000000, -32(%rbp)
-	jle	.L3
+	movl	$300000003, %esi
 	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	printf
+	movl	$.LC2, %edi
 	call	puts
-	leave
-	.cfi_def_cfa 7, 8
+	addq	$8, %rsp
+	.cfi_def_cfa_offset 8
 	ret
 	.cfi_endproc
-.LFE0:
+.LFE11:
 	.size	main, .-main
 	.ident	"GCC: (GNU) 4.4.7 20120313 (Red Hat 4.4.7-3)"
 	.section	.note.GNU-stack,"",@progbits
