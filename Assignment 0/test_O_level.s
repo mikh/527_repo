89839e52289f3c07b25b82152033c3bbef964edd
mikh <mikha.andreev@gmail.com>
2015-01-29 13:18:56 -0500

    Assembly of test_O_level

---
+++ Assignment 0/test_O_level.s
@@ -0,0 +1,26 @@
+	.file	"test_O_level.c"
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.LC0:
+	.string	"\n Starting a loop "
+.LC1:
+	.string	"\n done "
+	.text
+.globl main
+	.type	main, @function
+main:
+.LFB11:
+	.cfi_startproc
+	subq	$8, %rsp
+	.cfi_def_cfa_offset 16
+	movl	$.LC0, %edi
+	call	puts
+	movl	$.LC1, %edi
+	call	puts
+	addq	$8, %rsp
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE11:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.4.7 20120313 (Red Hat 4.4.7-3)"
+	.section	.note.GNU-stack,"",@progbits
