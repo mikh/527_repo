aadeab3dd8b204526555883087157eb75bb5e3b3
Mikhail Andreev <mikha.andreev@gmail.com>
2015-02-15 21:38:17 -0500

    [EC330] part 3c ii testing

--- EC330/HW2/main_p3.cpp
+++ EC330/HW2/main_p3.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <cstdio>
 #include <list>
+#include <deque>
 #include <sys/time.h>
 #include <sys/resource.h>
 
@@ -37,9 +38,10 @@ int main(){
         long rss_s, rss_e, ixrss_s, ixrss_e, idrss_s, idrss_e, isrss_s, isrss_e;
 
 	bigNumT<vector<int> > foo("81172150");
-       // bigNumT<deque<int> > foo2("81172150");
-       // bigNumT<list<int> > foo3("81172150");
-	
+        bigNumT<deque<int> > foo2("81172150");
+        bigNumT<list<int> > foo3("81172150");
+	cout<<"vector"<<endl;
+
         getrusage(RUSAGE_SELF, &usage);
         start = usage.ru_stime;
         rss_s = usage.ru_maxrss;
@@ -55,6 +57,54 @@ int main(){
         isrss_e = usage.ru_isrss;
 
 	cout << result.print() << endl;
+
+        cout<<(end.tv_sec*1000000+end.tv_usec) - (start.tv_sec*1000000+start.tv_usec)<<endl;
+        cout<<rss_e - rss_s<<endl;
+        cout<<ixrss_e - ixrss_s <<endl;
+        cout<<idrss_e - idrss_s << endl;
+        cout<<isrss_e - isrss_s << endl;
+
+        cout<<endl<<endl<<"deque"<<endl;
+
+         getrusage(RUSAGE_SELF, &usage);
+        start = usage.ru_stime;
+        rss_s = usage.ru_maxrss;
+        ixrss_s = usage.ru_ixrss;
+        idrss_s = usage.ru_idrss;
+        isrss_s = usage.ru_isrss;
+        bigNumT<deque<int> > result2 = CollatzCount(foo2);
+        getrusage(RUSAGE_SELF, &usage);
+        end = usage.ru_stime;
+        rss_e = usage.ru_maxrss;
+        ixrss_e = usage.ru_ixrss;
+        idrss_e = usage.ru_idrss;
+        isrss_e = usage.ru_isrss;
+
+        cout << result2.print() << endl;
+
+        cout<<(end.tv_sec*1000000+end.tv_usec) - (start.tv_sec*1000000+start.tv_usec)<<endl;
+        cout<<rss_e - rss_s<<endl;
+        cout<<ixrss_e - ixrss_s <<endl;
+        cout<<idrss_e - idrss_s << endl;
+        cout<<isrss_e - isrss_s << endl;
+
+                cout<<endl<<endl<<"list"<<endl;
+
+         getrusage(RUSAGE_SELF, &usage);
+        start = usage.ru_stime;
+        rss_s = usage.ru_maxrss;
+        ixrss_s = usage.ru_ixrss;
+        idrss_s = usage.ru_idrss;
+        isrss_s = usage.ru_isrss;
+        bigNumT<list<int> > result3 = CollatzCount(foo3);
+        getrusage(RUSAGE_SELF, &usage);
+        end = usage.ru_stime;
+        rss_e = usage.ru_maxrss;
+        ixrss_e = usage.ru_ixrss;
+        idrss_e = usage.ru_idrss;
+        isrss_e = usage.ru_isrss;
+
+        cout << result3.print() << endl;
 
         cout<<(end.tv_sec*1000000+end.tv_usec) - (start.tv_sec*1000000+start.tv_usec)<<endl;
         cout<<rss_e - rss_s<<endl;
