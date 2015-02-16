722dfb7b24f1ba2738335aa520552c95d94001b4
Mikhail Andreev <mikha.andreev@gmail.com>
2015-02-15 23:36:44 -0500

    Working on part 3 c iii

---
+++ EC330/HW2/student.h
@@ -0,0 +1,30 @@
+
+#ifndef STUDENT_H
+#define STUDENT_H
+
+
+#include <iostream>
+#include <iterator>
+
+using namespace std;
+
+int BASE_SIZE = 10000;
+
+
+class student{
+public:
+	student();
+	void push_back(int value);
+	void insert(int value);
+	iterator 
+private:
+	int *digit_array;
+};
+
+
+
+
+#endif
+
+
+
