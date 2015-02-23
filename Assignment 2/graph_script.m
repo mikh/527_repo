e7d3ce17302aef26ea1c26700c29202559d5cc70
Mikhail Andreev <mikha.andreev@gmail.com>
2015-02-23 13:44:06 -0500

    question 1 done

--- Assignment 2/graph_script.m
+++ Assignment 2/graph_script.m
@@ -158,3 +158,43 @@ title('All combines int and *');
 legend('combine1', 'combine2', 'combine3', 'combine4', 'combine5', 'combine6', 'combine7');
 hold off
 
+%% part b: loop unrolling
+
+filename = 'q1_data_double_multiply.txt';
+delimiterIn = ',';
+q1_data = importdata(filename, delimiterIn);
+
+
+figure(6);
+hold on
+scatter(q1_data(:,1), q1_data(:,9)./q1_data(:,1), 5, 'y');
+scatter(q1_data(:,1), q1_data(:,10)./q1_data(:,1), 5, 'm');
+scatter(q1_data(:,1), q1_data(:,11)./q1_data(:,1), 5, 'c');
+scatter(q1_data(:,1), q1_data(:,12)./q1_data(:,1), 5, 'r');
+xlabel('data elements');
+ylabel('cycles');
+title('Loop unrolling');
+legend('5x', '6x', '8x', '10x');
+hold off
+
+%% part c: with parallelization and association
+
+
+
+filename = 'q1_data_double_multiply.txt';
+delimiterIn = ',';
+q1_data = importdata(filename, delimiterIn);
+
+
+figure(7);
+hold on
+scatter(q1_data(:,1), q1_data(:,9)./q1_data(:,1), 5, 'y');
+scatter(q1_data(:,1), q1_data(:,10)./q1_data(:,1), 5, 'm');
+scatter(q1_data(:,1), q1_data(:,11)./q1_data(:,1), 5, 'c');
+scatter(q1_data(:,1), q1_data(:,12)./q1_data(:,1), 5, 'r');
+scatter(q1_data(:,1), q1_data(:,13)./q1_data(:,1), 5, 'b');
+xlabel('data elements');
+ylabel('cycles');
+title('Loop unrolling');
+legend('5x', '6x', '8x', '10x', 'parallelized');
+hold off
