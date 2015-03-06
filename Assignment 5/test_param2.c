56c82ec71c184d193f66651c3a197c8f65ac7897
Mikhail Andreev <mikhail.andreev@pemicro.com>
2015-03-06 12:03:28 -0500

    task 9 code

--- Assignment 5/test_param2.c
+++ Assignment 5/test_param2.c
@@ -14,9 +14,6 @@ void *work(void *i)
   long int j, k;
   int f = *((int*)(i));  // get the value being pointed to
   int *g = (int*)(i);    // get the pointer itself
-  printf("g = %lu - %d\n", g, *g);
-  *g = (*g) + 89;
-  printf("g = %lu - %d\n", g, *g);
 
   for (j; j < 10000000; j++) k += j;  // busy work
 
@@ -29,14 +26,15 @@ void *work(void *i)
 /*************************************************************************/
 int main(int argc, char *argv[])
 {
-  int arg,i,j,k,m;   	              /* Local variables. */
+  int arg,i,j,k,m, t, len = 0;   	              /* Local variables. */
   pthread_t id[NUM_THREADS];
 
   for (i = 0; i < NUM_THREADS; ++i) {
-    if (pthread_create(&id[i], NULL, work, (void *)(&i))) {
+    if ((i < NUM_THREADS) && pthread_create(&id[i], NULL, work, (void *)(&i))) {
       printf("ERROR creating the thread\n");
       exit(19);
     }
+    len++;
   }
 
   for (j; j < 100000000; j++) k += j;  // busy work
@@ -45,8 +43,8 @@ int main(int argc, char *argv[])
 	 pthread_self(), i);
 
 
-  for (i = 0; i < NUM_THREADS; i++) {
-    if (pthread_join(id[i],NULL)){ exit(19);
+  for (t = 0; t < len; t++) {
+    if (pthread_join(id[t],NULL)){ exit(19);
     }
   }
 
