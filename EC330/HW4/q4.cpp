5850336757a720fac496750929b40af5cfc5d41b
Mikhail Andreev <mikha.andreev@gmail.com>
2015-03-16 18:15:31 -0400

    q4 code

--- EC330/HW4/q4.cpp
+++ EC330/HW4/q4.cpp
@@ -109,7 +109,7 @@ vector<string> brute_force_5(vector<string> hashes){
 	printf("%d\n", distance);
 	double total_iters = (float)distance*(float)distance*(float)distance*(float)distance*(float)distance;
 	double current_iteration = 0;
-	double percentage = total_iters / 10000, percentage_val = percentage;
+	double percentage = total_iters / 100000, percentage_val = percentage;
 	printf("%f\n", total_iters);
 	for(char a = START_CHAR; a <= END_cHAR; a++){
 		for(char b = START_CHAR; b <= END_cHAR; b++){
