bde6ce2c711f144673ff289b5dc255c7521ee8e8
Mikhail Andreev <mikha.andreev@gmail.com>
2015-03-16 17:10:38 -0400

    q4 code

--- EC330/HW4/q4.cpp
+++ EC330/HW4/q4.cpp
@@ -11,7 +11,7 @@ const char START_CHAR = '!';
 const char END_cHAR = '~';
 const string P1_FILENAME = "p1_hashes.txt";
 const string P2_FILENAME = "p2_hashes.txt";
-const int MAX_ITERATIONS = 1000;
+const int MAX_ITERATIONS = 50000;
 const int P2_LENGTH = 5;
 
 const string P2_RAINBOW_TABLE = "table.txt";
