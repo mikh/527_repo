93eba69d53b1cdfc1c45c024ca7aceaa46112d99
Mikhail Andreev <mikha.andreev@gmail.com>
2015-03-22 16:38:59 -0400

    hw 6 q1 code

--- Assignment 6/graph_script.m
+++ Assignment 6/graph_script.m
@@ -3,10 +3,11 @@ filenames = {'q1_data_O_05_2_S_10.txt',
     'q1_data_O_05_2_S_20.txt',
     'q1_data_O_05_2_S_30.txt', 
     'q1_data_O_05_2_S_40.txt',
+    'q1_data_O_15_2_S_100.txt',
     'q1_data_O_05_2_S_200.txt'
     };
 titles = {'Size 10', 'Size 20', 'Size 30', 'Size 40', 'Size 200'};
-colors = ['b', 'r', 'g', 'k', 'c'];
+colors = ['b', 'r', 'g', 'k','m', 'c'];
 
 figure(1);
 hold on
