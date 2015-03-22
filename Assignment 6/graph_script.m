d7b9c8cdb26e5f1ab18e3b71b292af56f4299cd7
Mikhail Andreev <mikha.andreev@gmail.com>
2015-03-22 17:24:06 -0400

    hw 6 q1 code

--- Assignment 6/graph_script.m
+++ Assignment 6/graph_script.m
@@ -6,7 +6,7 @@ filenames = {'q1_data_O_05_2_S_10.txt',
     'q1_data_O_15_2_S_100.txt',
     'q1_data_O_05_2_S_200.txt'
     };
-titles = {'Size 10', 'Size 20', 'Size 30', 'Size 40', 'Size 200'};
+titles = {'Size 10', 'Size 20', 'Size 30', 'Size 40','Size 100', 'Size 200'};
 colors = ['b', 'r', 'g', 'k','m', 'c'];
 
 figure(1);
@@ -16,9 +16,11 @@ ylabel('Iterations to convergence');
 title('Omega vs. iterations for different array sizes');
 delimiterIn = ' ';
 for i = 1:length(filenames)
-    disp(filenames(i));
     data = importdata(filenames{i}, delimiterIn);
     scatter(data(:,1),data(:,2),5,colors(i));
+    [mi, ind] = min(data(:,2));
+    mi_ome = data(ind, 1);
+    fprintf('Minimum Omega for %s is %f\n',titles{i}, mi_ome);
 end
 legend(titles);
 
