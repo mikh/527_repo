fbf69de217ca5353d029a56d67338a951224f032
Mikhail Andreev <mikha.andreev@gmail.com>
2015-03-30 17:32:42 -0400

    hw 7 q3 code

--- Assignment 7/test_mmm_inter_omp.c
+++ Assignment 7/test_mmm_inter_omp.c
@@ -11,8 +11,8 @@
 #define CPG 3.6           // Cycles per GHz -- Adjust to your computer
 
 #define BASE  0
-#define ITERS 10
-#define DELTA 20
+#define ITERS 50
+#define DELTA 10
 
 #define OPTIONS 1
 #define IDENT 0
