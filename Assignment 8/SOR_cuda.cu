84e15a1c007d24c73b6df8be7a9daab410a0d57f
Mikhail Andreev <mikha.andreev@gmail.com>
2015-04-06 16:04:23 -0400

    hw 8 q2 code

--- Assignment 8/SOR_cuda.cu
+++ Assignment 8/SOR_cuda.cu
@@ -75,7 +75,7 @@ int main(int argc, char **argv){
 
 
 	//transfer array to GPU memory
-	CUDA_SAFE_CALL(cudaMemcpy(g_A, h_A, MATRIX_SIZE, cudaMemcpyHostToDevice));
+/*	CUDA_SAFE_CALL(cudaMemcpy(g_A, h_A, MATRIX_SIZE, cudaMemcpyHostToDevice));
 	for(i = 0; i < MATRIX_SIZE; i++){	//NOTE: might have to use pointer stuff here
 		CUDA_SAFE_CALL(cudaMemcpy(g_A[i], h_A[i], MATRIX_SIZE, cudaMemcpyHostToDevice));
 	}
@@ -87,7 +87,7 @@ int main(int argc, char **argv){
 
 	//check for errors during launch
 	CUDA_SAFE_CALL(cudaPeekAtLastError());
-
+*/
 	//transfer results back to host
 	CUDA_SAFE_CALL(cudaMemcpy(h_A, g_A, MATRIX_SIZE, cudaMemcpyDeviceToHost));
 	for(i = 0; i < MATRIX_SIZE; i++){	//NOTE: might have to use pointer stuff here
