72c17a38365f6d00adb5f2221ec68d63b57ceae5
Mikhail Andreev <mikha.andreev@gmail.com>
2015-04-19 21:57:25 -0400

    hw 9 q1 code

--- Assignment 9/MMM_cuda.cu
+++ Assignment 9/MMM_cuda.cu
@@ -26,7 +26,7 @@ inline void gpuAssert(cudaError_t code, char *file, int line, bool abort=true)
 
 #define NN 100		//matrix size
 
-#define THREADS_PER_BLOCK 32
+#define THREADS_PER_BLOCK 16
 
 #define THREADS_PER_BLOCK_Y 16
 #define NUM_BLOCKS_X 16
